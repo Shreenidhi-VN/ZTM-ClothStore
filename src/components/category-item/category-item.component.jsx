b9e4b75065f438fb79c3034437179f9e438a0623
shreenidhi vn <shreenidhi.vpuranagesh@gmail.com>
2023-04-22 12:41:56 +0530

    Fixed issue

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -2,13 +2,16 @@ import "./category-item.styles.scss";
 
 const CategoryItem = ({ category }) => {
   const { imageUrl, title } = category;
-  console.log(title, imageUrl);
+
   return (
     <div className="category-container">
       <div
-        className="category-body-container"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      >
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
